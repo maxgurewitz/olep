6369863aeab514e6ffd3db4edd519384823f682b
maxgurewitz <maxgurewitz@gmail.com>
2019-12-27 18:41:15 -0800

    create helm chart

--- pkg/request_handler.go
+++ pkg/request_handler.go
@@ -4,9 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// InitRequestHandler starts server responsible for handling payment requests.
 func InitRequestHandler() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	r.POST("/api/v1/payments", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
